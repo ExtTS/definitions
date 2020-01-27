227d9fb30aed708e4972f1bb8954e612219a4800
Tom Flídr <tomflidr@gmail.com>
2020-01-27 11:31:05 +0100

    version 6.2.0 classic - tested, OK

---
+++ 6.2.0-classic-all-packages/ext-6.2.0-classic-unknown.d.ts
@@ -0,0 +1,17 @@
+/**
+ * Ext.JS TypeScript empty definitions
+ * for unknown types in packages:
+ * 
+ * @version 6.2.0
+ * @toolkit classic
+ * @package unknown
+ */
+
+declare namespace Ext.exporter.file {
+	/** Ext.grid.column.Action, Ext.grid.column.Action, Ext.grid.column.Boolean, Ext.grid.column.Boolean, Ext.grid.column.Check, Ext.grid.column.Check, Ext.grid.column.Column, Ext.grid.column.Column, Ext.grid.column.Date, Ext.grid.column.Date, Ext.grid.column.Number, Ext.grid.column.Number, Ext.grid.column.RowNumberer, Ext.grid.column.RowNumberer, Ext.grid.column.Template, Ext.grid.column.Template, Ext.grid.column.Widget, Ext.grid.column.Widget, Ext.tree.Column, Ext.tree.Column, Ext.grid.column.Action, Ext.grid.column.Action, Ext.grid.column.Boolean, Ext.grid.column.Boolean, Ext.grid.column.Check, Ext.grid.column.Check, Ext.grid.column.Column, Ext.grid.column.Column, Ext.grid.column.Date, Ext.grid.column.Date, Ext.grid.column.Number, Ext.grid.column.Number, Ext.grid.column.RowNumberer, Ext.grid.column.RowNumberer, Ext.grid.column.Template, Ext.grid.column.Template, Ext.grid.column.Widget, Ext.grid.column.Widget, Ext.tree.Column, Ext.tree.Column */
+	class Style {}
+}
+declare namespace Ext.util {
+	/** Ext.util.Point.methodParams.alignTo.Options, Ext.util.Region.methodParams.alignTo.Options, Ext.util.Point.methodParams.alignTo.Options, Ext.util.Region.methodParams.alignTo.Options */
+	class Position {}
+}
