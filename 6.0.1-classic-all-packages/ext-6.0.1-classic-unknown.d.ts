b47398a38b33a207412597258e5b2f33bdc9c8dc
Tom Flídr <tomflidr@gmail.com>
2020-01-27 13:08:56 +0100

    v6.0.1 types updates - added overrides into core package

--- 6.0.1-classic-all-packages/ext-6.0.1-classic-unknown.d.ts
+++ 6.0.1-classic-all-packages/ext-6.0.1-classic-unknown.d.ts
@@ -7,7 +7,3 @@
  * @package unknown
  */
 
-declare namespace Ext.draw.sprite {
-	/** Ext.fx.target.Component.constructor, Ext.fx.target.CompositeSprite.constructor, Ext.fx.target.Element.constructor, Ext.fx.target.ElementCSS.constructor, Ext.fx.target.Target.constructor, Ext.fx.target.Sprite.constructor, Ext.fx.target.Component.constructor, Ext.fx.target.CompositeSprite.constructor, Ext.fx.target.Element.constructor, Ext.fx.target.ElementCSS.constructor, Ext.fx.target.Target.constructor, Ext.fx.target.Sprite.constructor */
-	class Sprite {}
-}
