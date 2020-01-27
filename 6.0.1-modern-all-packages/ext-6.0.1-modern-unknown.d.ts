31420e7929d2ce20c084e5510dc7f513230fefd3
Tom Flídr <tomflidr@gmail.com>
2020-01-27 09:01:08 +0100

    Removed not working type definitions and added only working definitions.

--- 6.0.1-modern-all-packages/ext-6.0.1-modern-unknown.d.ts
+++ 6.0.1-modern-all-packages/ext-6.0.1-modern-unknown.d.ts
@@ -7,13 +7,15 @@
  * @package unknown
  */
 
+/** Ext.device.filesystem.Chrome.methodParams.requestFileSystem.Config.failure, Ext.device.filesystem.Cordova.methodParams.download.Config.failure, Ext.device.filesystem.Cordova.methodParams.readMetadata.Config.failure, Ext.device.filesystem.DirectoryEntry.methodParams.getEntry.Config.failure, Ext.device.filesystem.Cordova.methodParams.upload.Config.failure, Ext.device.filesystem.Cordova.methodParams.writeMetadata.Config.failure, Ext.device.filesystem.FileEntry.methodParams.getEntry.Config.failure, Ext.device.filesystem.Chrome.methodParams.requestFileSystem.Config.failure, Ext.device.filesystem.Cordova.methodParams.download.Config.failure, Ext.device.filesystem.Cordova.methodParams.readMetadata.Config.failure, Ext.device.filesystem.DirectoryEntry.methodParams.getEntry.Config.failure, Ext.device.filesystem.Cordova.methodParams.upload.Config.failure, Ext.device.filesystem.Cordova.methodParams.writeMetadata.Config.failure, Ext.device.filesystem.FileEntry.methodParams.getEntry.Config.failure */
+declare class FileError {}
+/** Ext.device.filesystem.Cordova, Ext.device.filesystem.Cordova, Ext.device.filesystem.Cordova, Ext.device.filesystem.Cordova */
+declare class FileTransfer {}
+/** Ext.device.storage.HTML5.Database.transaction, Ext.device.storage.HTML5.Database.transaction, Ext.device.storage.HTML5.Database.transaction, Ext.device.storage.HTML5.Database.transaction */
+declare class SQLStatement {}
 declare namespace Ext {
-	/** Ext.Button.release, Ext.Button.tap, Ext.dataview.List.disclose, Ext.grid.Grid.disclose, Ext.Mask.tap, Ext.LoadMask.tap, Ext.slider.Slider.drag, Ext.slider.Slider.dragend, Ext.slider.Slider.dragstart, Ext.slider.Toggle.drag, Ext.slider.Toggle.dragend, Ext.slider.Toggle.dragstart, Ext.tab.Tab.release, Ext.tab.Tab.tap, Ext.Button.release, Ext.Button.tap, Ext.dataview.List.disclose, Ext.grid.Grid.disclose, Ext.Mask.tap, Ext.LoadMask.tap, Ext.slider.Slider.drag, Ext.slider.Slider.dragend, Ext.slider.Slider.dragstart, Ext.slider.Toggle.drag, Ext.slider.Toggle.dragend, Ext.slider.Toggle.dragstart, Ext.tab.Tab.release, Ext.tab.Tab.tap */
+	/** Ext.Button.release, Ext.Button.tap, Ext.dataview.List.disclose, Ext.grid.Grid.disclose, Ext.LoadMask.tap, Ext.Mask.tap, Ext.slider.Slider.drag, Ext.slider.Slider.dragend, Ext.slider.Slider.dragstart, Ext.slider.Toggle.drag, Ext.slider.Toggle.dragend, Ext.slider.Toggle.dragstart, Ext.tab.Tab.release, Ext.tab.Tab.tap, Ext.Button.release, Ext.Button.tap, Ext.dataview.List.disclose, Ext.grid.Grid.disclose, Ext.LoadMask.tap, Ext.Mask.tap, Ext.slider.Slider.drag, Ext.slider.Slider.dragend, Ext.slider.Slider.dragstart, Ext.slider.Toggle.drag, Ext.slider.Toggle.dragend, Ext.slider.Toggle.dragstart, Ext.tab.Tab.release, Ext.tab.Tab.tap */
 	class EventObject {}
-}
-declare namespace Ext.dataview.component {
-	/** Ext.dataview.List, Ext.grid.Grid, Ext.dataview.List, Ext.grid.Grid */
-	class (Simple)ListItem {}
 }
 declare namespace Ext.draw {
 	/** Ext.draw.Container */
@@ -28,4 +30,10 @@ declare namespace Ext.chart {
 declare namespace Ext.tree {
 	/** Ext.data.NodeInterface, Ext.data.NodeInterface */
 	class Panel {}
+}
+declare namespace google.maps {
+	/** Ext.BingMap.centerchange, Ext.Map.setMapCenter, Ext.Map.centerchange, Ext.BingMap.centerchange, Ext.Map.setMapCenter, Ext.Map.centerchange */
+	class LatLng {}
+	/** Ext.BingMap, Ext.BingMap, Ext.BingMap.setMap, Ext.BingMap.centerchange, Ext.BingMap.maprender, Ext.BingMap.typechange, Ext.BingMap.zoomchange, Ext.Map, Ext.Map, Ext.Map.setMap, Ext.Map.centerchange, Ext.Map.maprender, Ext.Map.typechange, Ext.Map.zoomchange, Ext.BingMap, Ext.BingMap, Ext.BingMap.setMap, Ext.BingMap.centerchange, Ext.BingMap.maprender, Ext.BingMap.typechange, Ext.BingMap.zoomchange, Ext.Map, Ext.Map, Ext.Map.setMap, Ext.Map.centerchange, Ext.Map.maprender, Ext.Map.typechange, Ext.Map.zoomchange */
+	class Map {}
 }
