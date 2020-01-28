df9d42bcac07240d4ab10b619332e78d7fe91ab2
Tom Flídr <tomflidr@gmail.com>
2020-01-28 13:34:08 +0100

    definitions for version 5.0.1

---
+++ 5.0.1-all-packages/ext-5.0.1-unknown.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Ext.JS TypeScript empty definitions
+ * for unknown types in packages:
+ * 
+ * @version 5.0.1
+ * @package unknown
+ */
+
+declare namespace Ext.data {
+	/** Ext.data.reader.Array, Ext.data.reader.Json, Ext.data.reader.Reader, Ext.data.reader.Xml, Ext.grid.property.Reader, Ext.data.reader.Array, Ext.data.reader.Json, Ext.data.reader.Reader, Ext.data.reader.Xml, Ext.grid.property.Reader */
+	class Result {}
+}
