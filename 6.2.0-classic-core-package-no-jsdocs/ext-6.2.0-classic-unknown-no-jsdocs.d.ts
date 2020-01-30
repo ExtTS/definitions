0ef569c89a86a702c1e44298617985f31e85d01b
Tom Flídr <tomflidr@gmail.com>
2020-01-30 12:08:29 +0100

    v6.2.0 classic - added definitions with no private members, @inheritdoc tags and with/without jsdocs

---
+++ 6.2.0-classic-core-package-no-jsdocs/ext-6.2.0-classic-unknown-no-jsdocs.d.ts
@@ -0,0 +1,21 @@
+/**
+ * Ext.JS TypeScript empty definitions
+ * for unknown types in packages:
+ * 
+ * @version 6.2.0
+ * @toolkit classic
+ * @package unknown
+ */
+
+declare namespace Ext.draw.sprite {
+	/** Ext.fx.target.Component.constructor, Ext.fx.target.CompositeSprite.constructor, Ext.fx.target.Element.constructor, Ext.fx.target.ElementCSS.constructor, Ext.fx.target.Sprite.constructor, Ext.fx.target.Target.constructor, Ext.fx.target.Component.constructor, Ext.fx.target.CompositeSprite.constructor, Ext.fx.target.Element.constructor, Ext.fx.target.ElementCSS.constructor, Ext.fx.target.Sprite.constructor, Ext.fx.target.Target.constructor */
+	class Sprite {}
+}
+declare namespace Ext.exporter.file {
+	/** Ext.grid.column.Action, Ext.grid.column.Action, Ext.grid.column.Boolean, Ext.grid.column.Boolean, Ext.grid.column.Check, Ext.grid.column.Check, Ext.grid.column.Column, Ext.grid.column.Column, Ext.grid.column.Date, Ext.grid.column.Date, Ext.grid.column.Number, Ext.grid.column.Number, Ext.grid.column.RowNumberer, Ext.grid.column.RowNumberer, Ext.grid.column.Template, Ext.grid.column.Template, Ext.grid.column.Widget, Ext.grid.column.Widget, Ext.tree.Column, Ext.tree.Column, Ext.grid.column.Action, Ext.grid.column.Action, Ext.grid.column.Boolean, Ext.grid.column.Boolean, Ext.grid.column.Check, Ext.grid.column.Check, Ext.grid.column.Column, Ext.grid.column.Column, Ext.grid.column.Date, Ext.grid.column.Date, Ext.grid.column.Number, Ext.grid.column.Number, Ext.grid.column.RowNumberer, Ext.grid.column.RowNumberer, Ext.grid.column.Template, Ext.grid.column.Template, Ext.grid.column.Widget, Ext.grid.column.Widget, Ext.tree.Column, Ext.tree.Column */
+	class Style {}
+}
+declare namespace Ext.util {
+	/** Ext.util.Point.methodParams.alignTo.Options, Ext.util.Region.methodParams.alignTo.Options, Ext.util.Point.methodParams.alignTo.Options, Ext.util.Region.methodParams.alignTo.Options */
+	class Position {}
+}
