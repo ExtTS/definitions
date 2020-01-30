2ae7d2d9ba0d8536249025d46434b2f46e58e81b
Tom Flídr <tomflidr@gmail.com>
2020-01-30 11:47:36 +0100

    v6.2.0 modern - definitions without private members and with @inheritdoc and with no jsdocs

---
+++ 6.2.0-modern-all-packages-no-jsdocs/ext-6.2.0-modern-unknown-no-jsdocs.d.ts
@@ -0,0 +1,67 @@
+/**
+ * Ext.JS TypeScript empty definitions
+ * for unknown types in packages:
+ * 
+ * @version 6.2.0
+ * @toolkit modern
+ * @package unknown
+ */
+
+/** Ext.device.filesystem.Chrome.methodParams.requestFileSystem.Config.failure, Ext.device.filesystem.Cordova.methodParams.download.Config.failure, Ext.device.filesystem.Cordova.methodParams.readMetadata.Config.failure, Ext.device.filesystem.Cordova.methodParams.upload.Config.failure, Ext.device.filesystem.Cordova.methodParams.writeMetadata.Config.failure, Ext.device.filesystem.DirectoryEntry.methodParams.getEntry.Config.failure, Ext.device.filesystem.FileEntry.methodParams.getEntry.Config.failure, Ext.device.filesystem.Chrome.methodParams.requestFileSystem.Config.failure, Ext.device.filesystem.Cordova.methodParams.download.Config.failure, Ext.device.filesystem.Cordova.methodParams.readMetadata.Config.failure, Ext.device.filesystem.Cordova.methodParams.upload.Config.failure, Ext.device.filesystem.Cordova.methodParams.writeMetadata.Config.failure, Ext.device.filesystem.DirectoryEntry.methodParams.getEntry.Config.failure, Ext.device.filesystem.FileEntry.methodParams.getEntry.Config.failure */
+declare class FileError {}
+/** Ext.device.filesystem.Cordova, Ext.device.filesystem.Cordova, Ext.device.filesystem.Cordova, Ext.device.filesystem.Cordova */
+declare class FileTransfer {}
+/** Ext.device.storage.HTML5.Database.transaction, Ext.device.storage.HTML5.Database.transaction, Ext.device.storage.HTML5.Database.transaction, Ext.device.storage.HTML5.Database.transaction */
+declare class SQLStatement {}
+declare namespace Ext {
+	/** Ext.Button.release, Ext.Button.tap, Ext.dataview.List.disclose, Ext.grid.Grid.disclose, Ext.grid.Tree.disclose, Ext.LoadMask.tap, Ext.Mask.tap, Ext.slider.Slider.drag, Ext.slider.Slider.dragend, Ext.slider.Slider.dragstart, Ext.slider.Toggle.drag, Ext.slider.Toggle.dragend, Ext.slider.Toggle.dragstart, Ext.tab.Tab.release, Ext.tab.Tab.tap, Ext.Button.release, Ext.Button.tap, Ext.dataview.List.disclose, Ext.grid.Grid.disclose, Ext.grid.Tree.disclose, Ext.LoadMask.tap, Ext.Mask.tap, Ext.slider.Slider.drag, Ext.slider.Slider.dragend, Ext.slider.Slider.dragstart, Ext.slider.Toggle.drag, Ext.slider.Toggle.dragend, Ext.slider.Toggle.dragstart, Ext.tab.Tab.release, Ext.tab.Tab.tap */
+	class EventObject {}
+}
+declare namespace Ext.button {
+	/** Ext.SegmentedButton, Ext.SegmentedButton */
+	class Button {}
+}
+declare namespace Ext.draw {
+	/** Ext.draw.Container */
+	class ContainerBase {}
+}
+declare namespace Ext.exporter.file {
+	/** Ext.grid.column.Boolean, Ext.grid.column.Boolean, Ext.grid.column.Boolean, Ext.grid.column.Boolean, Ext.grid.column.Boolean.setExportStyle, Ext.grid.column.Boolean.setExportStyle, Ext.grid.column.Check, Ext.grid.column.Check, Ext.grid.column.Check, Ext.grid.column.Check, Ext.grid.column.Check.setExportStyle, Ext.grid.column.Check.setExportStyle, Ext.grid.column.Column, Ext.grid.column.Column, Ext.grid.column.Column, Ext.grid.column.Column, Ext.grid.column.Column.setExportStyle, Ext.grid.column.Column.setExportStyle, Ext.grid.column.Date, Ext.grid.column.Date, Ext.grid.column.Date, Ext.grid.column.Date, Ext.grid.column.Date.setExportStyle, Ext.grid.column.Date.setExportStyle, Ext.grid.column.Number, Ext.grid.column.Number, Ext.grid.column.Number, Ext.grid.column.Number, Ext.grid.column.Number.setExportStyle, Ext.grid.column.Number.setExportStyle, Ext.grid.column.RowNumberer, Ext.grid.column.RowNumberer, Ext.grid.column.RowNumberer, Ext.grid.column.RowNumberer, Ext.grid.column.RowNumberer.setExportStyle, Ext.grid.column.RowNumberer.setExportStyle, Ext.grid.column.Selection, Ext.grid.column.Selection, Ext.grid.column.Selection, Ext.grid.column.Selection, Ext.grid.column.Selection.setExportStyle, Ext.grid.column.Selection.setExportStyle, Ext.grid.column.Tree, Ext.grid.column.Tree, Ext.grid.column.Tree, Ext.grid.column.Tree, Ext.grid.column.Tree.setExportStyle, Ext.grid.column.Tree.setExportStyle, Ext.grid.column.Boolean, Ext.grid.column.Boolean, Ext.grid.column.Boolean, Ext.grid.column.Boolean, Ext.grid.column.Boolean.setExportStyle, Ext.grid.column.Boolean.setExportStyle, Ext.grid.column.Check, Ext.grid.column.Check, Ext.grid.column.Check, Ext.grid.column.Check, Ext.grid.column.Check.setExportStyle, Ext.grid.column.Check.setExportStyle, Ext.grid.column.Column, Ext.grid.column.Column, Ext.grid.column.Column, Ext.grid.column.Column, Ext.grid.column.Column.setExportStyle, Ext.grid.column.Column.setExportStyle, Ext.grid.column.Date, Ext.grid.column.Date, Ext.grid.column.Date, Ext.grid.column.Date, Ext.grid.column.Date.setExportStyle, Ext.grid.column.Date.setExportStyle, Ext.grid.column.Number, Ext.grid.column.Number, Ext.grid.column.Number, Ext.grid.column.Number, Ext.grid.column.Number.setExportStyle, Ext.grid.column.Number.setExportStyle, Ext.grid.column.RowNumberer, Ext.grid.column.RowNumberer, Ext.grid.column.RowNumberer, Ext.grid.column.RowNumberer, Ext.grid.column.RowNumberer.setExportStyle, Ext.grid.column.RowNumberer.setExportStyle, Ext.grid.column.Selection, Ext.grid.column.Selection, Ext.grid.column.Selection, Ext.grid.column.Selection, Ext.grid.column.Selection.setExportStyle, Ext.grid.column.Selection.setExportStyle, Ext.grid.column.Tree, Ext.grid.column.Tree, Ext.grid.column.Tree, Ext.grid.column.Tree, Ext.grid.column.Tree.setExportStyle, Ext.grid.column.Tree.setExportStyle */
+	class Style {}
+}
+declare namespace Ext.field {
+	/** Ext.field.trigger.Trigger.sort, Ext.field.trigger.Trigger, Ext.field.trigger.Trigger.sort, Ext.field.trigger.Trigger */
+	class Trigger {}
+}
+declare namespace Ext.form.field {
+	/** Ext.field.trigger.Trigger.weightComparator, Ext.field.trigger.Trigger.weightComparator, Ext.field.trigger.Trigger.weightComparator, Ext.field.trigger.Trigger.weightComparator */
+	class Trigger {}
+}
+declare namespace Ext.grid {
+	/** Ext.grid.column.Check.eventsParams.beforecheckchange.E, Ext.grid.column.Check.eventsParams.checkchange.E, Ext.grid.column.Selection.eventsParams.beforecheckchange.E, Ext.grid.column.Selection.eventsParams.checkchange.E, Ext.grid.column.Check.eventsParams.beforecheckchange.E, Ext.grid.column.Check.eventsParams.checkchange.E, Ext.grid.column.Selection.eventsParams.beforecheckchange.E, Ext.grid.column.Selection.eventsParams.checkchange.E */
+	class CellContext {}
+	/** Ext.grid.cell.Tree, Ext.grid.cell.Tree */
+	class TreeGrid {}
+}
+declare namespace Ext.tree {
+	/** Ext.data.NodeInterface, Ext.data.NodeInterface */
+	class Panel {}
+}
+declare namespace Ext.util {
+	/** Ext.util.Point.methodParams.alignTo.Options, Ext.util.Region.methodParams.alignTo.Options, Ext.util.Point.methodParams.alignTo.Options, Ext.util.Region.methodParams.alignTo.Options */
+	class Position {}
+}
+// Manually edited: 
+declare namespace ExtGlobalGoogle.maps {
+	/** Ext.ux.google.Map.setMapCenter, Ext.ux.google.Map.centerchange, Ext.ux.google.Map.setMapCenter, Ext.ux.google.Map.centerchange */
+	class LatLng {}
+	/** Ext.ux.google.Map, Ext.ux.google.Map, Ext.ux.google.Map.setMap, Ext.ux.google.Map.centerchange, Ext.ux.google.Map.maprender, Ext.ux.google.Map.typechange, Ext.ux.google.Map.zoomchange, Ext.ux.google.Map, Ext.ux.google.Map, Ext.ux.google.Map.setMap, Ext.ux.google.Map.centerchange, Ext.ux.google.Map.maprender, Ext.ux.google.Map.typechange, Ext.ux.google.Map.zoomchange */
+	class Map {}
+}
+declare namespace Ext.ux.google.maps {
+	/** Ext.ux.google.Map.setMapCenter, Ext.ux.google.Map.centerchange, Ext.ux.google.Map.setMapCenter, Ext.ux.google.Map.centerchange */
+	class LatLng extends ExtGlobalGoogle.maps.LatLng {}
+	/** Ext.ux.google.Map, Ext.ux.google.Map, Ext.ux.google.Map.setMap, Ext.ux.google.Map.centerchange, Ext.ux.google.Map.maprender, Ext.ux.google.Map.typechange, Ext.ux.google.Map.zoomchange, Ext.ux.google.Map, Ext.ux.google.Map, Ext.ux.google.Map.setMap, Ext.ux.google.Map.centerchange, Ext.ux.google.Map.maprender, Ext.ux.google.Map.typechange, Ext.ux.google.Map.zoomchange */
+	class Map extends ExtGlobalGoogle.maps.Map {}
+}
+
