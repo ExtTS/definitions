2a40426000be4371c0120ecb66575d92143d56cf
Tom Flídr <tomflidr@gmail.com>
2020-01-30 11:59:36 +0100

    v6.2.0 modern - definitions for core package only with no private members and with @inheritdoc tags, with and without jsdocs

---
+++ 6.2.0-modern-core-package/ext-6.2.0-modern-unknown.d.ts
@@ -0,0 +1,43 @@
+/**
+ * Ext.JS TypeScript empty definitions
+ * for unknown types in packages:
+ * 
+ * @version 6.2.0
+ * @toolkit modern
+ * @package unknown
+ */
+
+declare namespace Ext {
+	/** Ext.Button.release, Ext.Button.tap, Ext.dataview.List.disclose, Ext.grid.Grid.disclose, Ext.grid.Tree.disclose, Ext.LoadMask.tap, Ext.Mask.tap, Ext.slider.Slider.drag, Ext.slider.Slider.dragend, Ext.slider.Slider.dragstart, Ext.slider.Toggle.drag, Ext.slider.Toggle.dragend, Ext.slider.Toggle.dragstart, Ext.tab.Tab.release, Ext.tab.Tab.tap, Ext.Button.release, Ext.Button.tap, Ext.dataview.List.disclose, Ext.grid.Grid.disclose, Ext.grid.Tree.disclose, Ext.LoadMask.tap, Ext.Mask.tap, Ext.slider.Slider.drag, Ext.slider.Slider.dragend, Ext.slider.Slider.dragstart, Ext.slider.Toggle.drag, Ext.slider.Toggle.dragend, Ext.slider.Toggle.dragstart, Ext.tab.Tab.release, Ext.tab.Tab.tap */
+	class EventObject {}
+}
+declare namespace Ext.button {
+	/** Ext.SegmentedButton, Ext.SegmentedButton */
+	class Button {}
+}
+declare namespace Ext.exporter.file {
+	/** Ext.grid.column.Boolean, Ext.grid.column.Boolean, Ext.grid.column.Boolean, Ext.grid.column.Boolean, Ext.grid.column.Boolean.setExportStyle, Ext.grid.column.Boolean.setExportStyle, Ext.grid.column.Check, Ext.grid.column.Check, Ext.grid.column.Check, Ext.grid.column.Check, Ext.grid.column.Check.setExportStyle, Ext.grid.column.Check.setExportStyle, Ext.grid.column.Column, Ext.grid.column.Column, Ext.grid.column.Column, Ext.grid.column.Column, Ext.grid.column.Column.setExportStyle, Ext.grid.column.Column.setExportStyle, Ext.grid.column.Date, Ext.grid.column.Date, Ext.grid.column.Date, Ext.grid.column.Date, Ext.grid.column.Date.setExportStyle, Ext.grid.column.Date.setExportStyle, Ext.grid.column.Number, Ext.grid.column.Number, Ext.grid.column.Number, Ext.grid.column.Number, Ext.grid.column.Number.setExportStyle, Ext.grid.column.Number.setExportStyle, Ext.grid.column.RowNumberer, Ext.grid.column.RowNumberer, Ext.grid.column.RowNumberer, Ext.grid.column.RowNumberer, Ext.grid.column.RowNumberer.setExportStyle, Ext.grid.column.RowNumberer.setExportStyle, Ext.grid.column.Selection, Ext.grid.column.Selection, Ext.grid.column.Selection, Ext.grid.column.Selection, Ext.grid.column.Selection.setExportStyle, Ext.grid.column.Selection.setExportStyle, Ext.grid.column.Tree, Ext.grid.column.Tree, Ext.grid.column.Tree, Ext.grid.column.Tree, Ext.grid.column.Tree.setExportStyle, Ext.grid.column.Tree.setExportStyle, Ext.grid.column.Boolean, Ext.grid.column.Boolean, Ext.grid.column.Boolean, Ext.grid.column.Boolean, Ext.grid.column.Boolean.setExportStyle, Ext.grid.column.Boolean.setExportStyle, Ext.grid.column.Check, Ext.grid.column.Check, Ext.grid.column.Check, Ext.grid.column.Check, Ext.grid.column.Check.setExportStyle, Ext.grid.column.Check.setExportStyle, Ext.grid.column.Column, Ext.grid.column.Column, Ext.grid.column.Column, Ext.grid.column.Column, Ext.grid.column.Column.setExportStyle, Ext.grid.column.Column.setExportStyle, Ext.grid.column.Date, Ext.grid.column.Date, Ext.grid.column.Date, Ext.grid.column.Date, Ext.grid.column.Date.setExportStyle, Ext.grid.column.Date.setExportStyle, Ext.grid.column.Number, Ext.grid.column.Number, Ext.grid.column.Number, Ext.grid.column.Number, Ext.grid.column.Number.setExportStyle, Ext.grid.column.Number.setExportStyle, Ext.grid.column.RowNumberer, Ext.grid.column.RowNumberer, Ext.grid.column.RowNumberer, Ext.grid.column.RowNumberer, Ext.grid.column.RowNumberer.setExportStyle, Ext.grid.column.RowNumberer.setExportStyle, Ext.grid.column.Selection, Ext.grid.column.Selection, Ext.grid.column.Selection, Ext.grid.column.Selection, Ext.grid.column.Selection.setExportStyle, Ext.grid.column.Selection.setExportStyle, Ext.grid.column.Tree, Ext.grid.column.Tree, Ext.grid.column.Tree, Ext.grid.column.Tree, Ext.grid.column.Tree.setExportStyle, Ext.grid.column.Tree.setExportStyle */
+	class Style {}
+}
+declare namespace Ext.field {
+	/** Ext.field.trigger.Trigger.sort, Ext.field.trigger.Trigger, Ext.field.trigger.Trigger.sort, Ext.field.trigger.Trigger */
+	class Trigger {}
+}
+declare namespace Ext.form.field {
+	/** Ext.field.trigger.Trigger.weightComparator, Ext.field.trigger.Trigger.weightComparator, Ext.field.trigger.Trigger.weightComparator, Ext.field.trigger.Trigger.weightComparator */
+	class Trigger {}
+}
+declare namespace Ext.grid {
+	/** Ext.grid.column.Check.eventsParams.beforecheckchange.E, Ext.grid.column.Check.eventsParams.checkchange.E, Ext.grid.column.Selection.eventsParams.beforecheckchange.E, Ext.grid.column.Selection.eventsParams.checkchange.E, Ext.grid.column.Check.eventsParams.beforecheckchange.E, Ext.grid.column.Check.eventsParams.checkchange.E, Ext.grid.column.Selection.eventsParams.beforecheckchange.E, Ext.grid.column.Selection.eventsParams.checkchange.E */
+	class CellContext {}
+	/** Ext.grid.cell.Tree, Ext.grid.cell.Tree */
+	class TreeGrid {}
+}
+declare namespace Ext.tree {
+	/** Ext.data.NodeInterface, Ext.data.NodeInterface */
+	class Panel {}
+}
+declare namespace Ext.util {
+	/** Ext.util.Point.methodParams.alignTo.Options, Ext.util.Region.methodParams.alignTo.Options, Ext.util.Point.methodParams.alignTo.Options, Ext.util.Region.methodParams.alignTo.Options */
+	class Position {}
+}
